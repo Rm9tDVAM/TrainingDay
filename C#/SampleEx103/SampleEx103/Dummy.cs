7928167346f362aaf05c6ff29bb0b467075f3e65
Rm9tDVAM <u1exite.znd@gmail.com>
2023-07-31 23:36:52 +0900

    add ex103

---
+++ C#/SampleEx103/SampleEx103/Dummy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx103
+{
+    class Dummy
+    {
+        public Dummy()
+        {
+            Console.WriteLine("コンストラクタ");
+        }
+        ~Dummy()
+        {
+            Console.WriteLine("デストラクタ");
+        }
+    }
+}
