631942ec8f49a2828088652bf0bcde655257a09d
Rm9tDVAM <u1exite.znd@gmail.com>
2023-07-31 19:27:03 +0900

    add 702

---
+++ C#/Sample702/Sample702/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample702
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Access a=new Access();
+            a.Data2 = 2;
+            a.ShowDates();
+            Console.WriteLine("a.data1 = {0}", a.Data1);
+        }
+    }
+}
