9dde79dafe70308aac914bcd20df30f40a9f1dd1
Rm9tDVAM <u1exite.znd@gmail.com>
2023-07-31 21:41:39 +0900

    deleted test,add 703

--- C#/SampleEx703/SampleEx703/Program.cs
+++ C#/SampleEx703/SampleEx703/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test2
+namespace SampleEx703
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person2 p = new Person2();
+            p.SetAgeAndName("山田 太郎", 26);
+            p.Age = 32;
+            Console.WriteLine("名前:{0} 年齢:{1}", p.Name, p.Age);
         }
     }
 }
--- C#/SampleEx703/SampleEx703/Properties/AssemblyInfo.cs
+++ C#/SampleEx703/SampleEx703/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般的な情報は、次の方法で制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("test2")]
+[assembly: AssemblyTitle("SampleEx703")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test2")]
+[assembly: AssemblyProduct("SampleEx703")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("7f243172-78bd-412e-b737-fe0b65ad6fa2")]
+[assembly: Guid("cf88175a-8b3d-47a8-b904-231a594d3468")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
