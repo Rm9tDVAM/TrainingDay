1181b7fd6db45a66c651b8bce8721d5a1c207d23
Rm9tDVAM <u1exite.znd@gmail.com>
2023-07-31 16:56:26 +0900

    add 602

--- C#/Sample601/Sample601/Program.cs
+++ C#/Sample601/Sample601/Program.cs
@@ -4,4 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 
-
+namespace Sample601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1, p2;
+            p1 = new Person();
+            p2 = new Person();
+            p1.name = "yamada tarou";
+            p1.age = 19;
+            p2.SetAgeAndName("satou hanako", 23);
+            p1.ShowAgeAndName();
+            p2.ShowAgeAndName();
+        }
+    }
+}
