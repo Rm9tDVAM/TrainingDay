1bb37145671fc009c8e4d246911b991b67f65738
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-01 22:49:58 +0900

    add Ex301

---
+++ C#/SampleEx301/SampleEx301/ExCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx301
+{
+    class ExCalculatro:Calculator
+    {
+        public void mul()
+        {
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+        }
+        public void div()
+        {
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+        }
+    }
+}
---
+++ C#/SampleEx301/SampleEx301/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c1 = new Calculator();
+            c1.Num1 = 10;
+            c1.Num2 = 3;
+            c1.add();
+            c1.sub();
+            ExCalculatro c2 = new ExCalculatro();
+            c2.Num1 = 10;
+            c2.Num2 = 3;
+            c2.add();
+            c2.sub();
+            c2.mul();
+            c2.div();
+        }
+    }
+}
