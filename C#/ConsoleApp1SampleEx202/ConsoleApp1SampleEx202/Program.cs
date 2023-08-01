7740f68de4742356bff7db5477f1924a67845fb4
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-01 12:48:47 +0900

    addEx202

---
+++ C#/ConsoleApp1SampleEx202/ConsoleApp1SampleEx202/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1SampleEx202
+{
+    internal class Program
+    {
+        private static int snum=100;
+        public int inum = 200;
+        public static void foo()
+        {
+            Console.WriteLine("fooメソッド (staticメソッド)");
+        }
+        public void bar()
+        {
+            Console.WriteLine("barメソッド(インスタンスメソッド)");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("pのインスタンスフィールド:inum={0}", p.inum);
+            Console.WriteLine("Programのクラスフィールド:snum={0}", snum);
+            foo();
+            p.bar();
+        }
+    }
+}
