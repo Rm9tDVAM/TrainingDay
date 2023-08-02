f08ba69a539bc10d9c6c637b98255dcd0df3861f
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-02 12:59:13 +0900

    add Ex303

---
+++ C#/SampleEx303/SampleEx303/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p=new Parent();
+            Parent c = new Child();
+            p.Foo();
+            c.Foo();
+        }
+    }
+}
