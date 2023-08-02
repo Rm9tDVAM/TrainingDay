d84e238467fc1052fd6656ba7775ffc7f5f24633
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-02 12:47:52 +0900

    add Ex302

---
+++ C#/SampleEx302/SampleEx302/Sub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx302
+{
+    class Sub:Super
+    {
+        public Sub()
+        {
+            Console.WriteLine("Subのコンストラクタ(引数なし");
+        }
+        public Sub(int param) : base(param)
+        {
+            Console.WriteLine("Subのコンストラクタ(引数:param={0}", param);
+        }
+        ~Sub()
+        {
+            Console.WriteLine("Subクラスのデストラクタ");
+        }
+    }
+}
