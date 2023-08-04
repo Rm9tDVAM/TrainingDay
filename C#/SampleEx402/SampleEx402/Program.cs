6340b3b8f463018b6e875ce0ca54de5c45c24cec
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-04 12:49:13 +0900

    add Ex501

---
+++ C#/SampleEx402/SampleEx402/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx402
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Crow c = new Crow();
+            Sparrow s = new Sparrow();
+            Console.Write(c.Name + " : ");
+            c.Sing();
+            Console.Write(s.Name + " : ");
+            s.Sing();
+        }
+    }
+}
---
+++ C#/SampleEx501/SampleEx501/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx501
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CallPhone cp = new CallPhone("hoge@email.com", "090-1234-5678");
+            cp.Call("011-123-4567");
+            cp.SendMail("fuga@email.com");
+            IPhone phone = (IPhone)cp;
+            phone.Call("011-987-6543");
+            IEmail mail = (IEmail)cp;
+            mail.SendMail("bar@emai.com");
+        }
+    }
+}
