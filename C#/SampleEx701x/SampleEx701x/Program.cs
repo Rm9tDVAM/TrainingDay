6459c0622b4bf1f75a845ab88e9948b0d84d0d97
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-06 23:15:18 +0900

    add Ex604,Ex701x

---
+++ C#/SampleEx701x/SampleEx701x/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx701x
+{
+    delegate void Operation(int a, int b);
+    class Calc
+    {
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+        }
+    }
+    internal class Program
+    {
+        static void Add(int a, int b)
+        {
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+        }
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+            Operation o1 = new Operation(Add);
+            Operation o2 = new Operation(c.Sub);
+            o1(2, 1);
+            o2(2, 1);
+        }
+    }
+}
