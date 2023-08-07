b7536f2b9c106fce1eb90f1ee31c3b6c40de9a90
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-07 22:38:02 +0900

    add 0807

---
+++ C#/230807/rankD_01/rankD_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rankD_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int line = int.Parse(Console.ReadLine());
+            for (int i = 0; i < line; i++)
+            {
+                Console.Write("a");
+            }
+        }
+    }
+}
