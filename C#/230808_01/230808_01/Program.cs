d7bd745d4b76d66deddde7905fcd561971b70896
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-08 12:56:31 +0900

    230808

---
+++ C#/230808_01/230808_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230808_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(array[0]) - int.Parse(array[1]));
+        }
+    }
+}
