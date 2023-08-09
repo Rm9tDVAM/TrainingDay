cf887df9d6289d5f5a352e293adf3725e2a04e8a
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-09 22:42:38 +0900

    230809

---
+++ C#/230809_01/230809_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230809_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var line=Console.ReadLine();
+            int ans;ans = 2;
+            for(int i = 1; i < int.Parse(line); i++)
+            {
+                ans = ans * 2;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
