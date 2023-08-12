404c7494b0cb0bd45a263cc55cfcc150745bfd0f
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-12 15:46:56 +0900

    add 230812_1-3

---
+++ C#/230812_2/230812_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230812_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int low=int.Parse(Console.ReadLine());
+            int high=int.Parse(Console.ReadLine());
+            Console.WriteLine(b-a);
+        }
+    }
+}
---
+++ C#/230812_3/230812_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230812_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineOne = int.Parse(args[0]);
+            int lineTwo = int.Parse(args[0]);
+            Console.WriteLine(lineOne+lineTwo);
+        }
+    }
+}
