2b7ba67b5065d7b417384d5628c32a0dba256460
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-13 16:18:45 +0900

    add 230813_01-10

---
+++ C#/230813_05/230813_05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230813_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1=Console.ReadLine();
+            string line2=Console.ReadLine();
+            Console.WriteLine(line1+"/"+line2);
+        }
+    }
+}
---
+++ C#/230813_07/230813_07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230813_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();  
+            Console.WriteLine(line+line+line);
+        }
+    }
+}
