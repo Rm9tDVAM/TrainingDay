8a021c9607071932dd935fe3a7a78ef542e1e579
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-14 19:29:51 +0900

    add

---
+++ C#/2308/0814/13/13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int line1=int.Parse(Console.ReadLine());
+            int line2=int.Parse(Console.ReadLine());
+            Console.WriteLine(100 + ( line1 * line2));
+        }
+    }
+}
