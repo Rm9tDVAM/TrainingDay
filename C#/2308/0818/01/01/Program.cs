cac9c0c27d2980a23aecf730da8d1b55307f94ce
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-19 14:30:25 +0900

    add

---
+++ C#/2308/0818/01/01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int line1 = int.Parse(Console.ReadLine());
+            int line2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(line1 - 100);
+            Console.WriteLine(line2 + 100);
+        }
+    }
+}
---
+++ C#/2308/0818/02/02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans = 0;
+            int count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                ans += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
