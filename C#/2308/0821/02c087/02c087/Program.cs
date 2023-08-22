61d56e3c153366f74df00bb718fbd6ac8fe1f327
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-22 13:00:35 +0900

    add

--- C#/2308/0821/02c087/02c087/Program.cs
+++ C#/2308/0821/02c087/02c087/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +15,40 @@ namespace _02c087
         static void Main(string[] args)
         {
             string line = Console.ReadLine();
-            ArrayList array = new ArrayList();
-            for (int i = line.Length -1 ; i >= 0; i--)
+            string rLine = Reverse(line);
+            int ans = int.Parse(line) + int.Parse(rLine);
+            if (CheckReverse(ans.ToString()))
             {
-                array.Add(i);
+                Console.WriteLine(ans);
             }
+            else
+            {
+                line = ans.ToString();
+                rLine = Reverse(line);
+                ans = int.Parse(line) + int.Parse(rLine);
+                Console.WriteLine(ans);
+            }
+        }
+        static Boolean CheckReverse(string ans)
+        {
+            string temp = Reverse(ans);
+            if (ans == temp)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static string Reverse(string line)
+        {
+            string temp = "";
+            for(int i = line.Length -1; 0 <= i ; i--)
+            {
+                temp += line.Substring(i,1);
+            }
+            return temp;
         }
     }
 }
