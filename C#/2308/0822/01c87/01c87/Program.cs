12abd339681b75936c4a9c145000bd0d39917cfc
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-23 13:02:50 +0900

    add

---
+++ C#/2308/0822/01c87/01c87/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01c87
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string rstr = "";
+            int temp = 0;
+            while (true)
+            {
+                rstr = ReverseString(str);
+                temp = int.Parse(str)+int.Parse(rstr);
+                str = temp.ToString();
+                if (ReverseCheck(str))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(str);
+        }
+        static Boolean ReverseCheck(string str)
+        {
+            string temp = ReverseString(str);
+            if (str == temp)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static string ReverseString(string str)
+        {
+            string temp = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                temp += str[i];
+
+            }
+            return temp;
+        }
+    }
+}
---
+++ C#/2308/0823/01c085/01c085/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01c085
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] line1 = Console.ReadLine().Split(' ');
+            // string line2 = Console.ReadLine();
+            string[] line1 = { "1", "3","2","6","3","5","5","6","2","6","0","4","5","2","4","2","1","2","4","0","4","2","2","5","0","2" };
+            string line2 = "abcabcabcabc";
+        }
+    }
+}
