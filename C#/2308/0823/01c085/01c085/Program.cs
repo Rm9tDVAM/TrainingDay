8421f952405319e5fa8c1f347146f5f82b31f6b6
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-30 12:59:04 +0900

    add

--- C#/2308/0823/01c085/01c085/Program.cs
+++ C#/2308/0823/01c085/01c085/Program.cs
@@ -11,11 +11,8 @@ namespace _01c085
     {
         static void Main(string[] args)
         {
-            // string[] line1 = Console.ReadLine().Split(' ');
-            string line = "1 3 2 6 3 5 5 6 2 6 0 4 5 2 4 2 1 2 4 0 4 2 2 5 0 2";
-            string[] line1 = line.Split(' ');
-            // string line2 = Console.ReadLine();
-            string line2 = "abcabcabcabc";
+            int[] line1 = Console.ReadLine().Split(' ').Select(a =>int.Parse(a)).ToArray();
+            string line2 = Console.ReadLine();
             var atoz = string.Concat(Enumerable.Range('a', 26).Select(c =>(char)c));
             Hashtable ht=new Hashtable();
             int temp;
@@ -25,18 +22,12 @@ namespace _01c085
             }
             for(int i=0;i<line2.Length ; i++)
             {
-                Console.Write(int.Parse((string)ht[line2.Substring(i, 1)]) );
-                if (int.Parse((string)ht[line2.Substring(i, 1)]) != 0)
+                if ((int)ht[line2.Substring(i, 1)] != 0)
                 {
-                    Console.WriteLine(line2.Substring(i,1));
-
-                    ht[line2.Substring(i, 1)] = (string)(object)(int.Parse((string)ht[line2.Substring(i, 1)]) - 1);;
+                    Console.Write(line2.Substring(i,1));
+                    ht[line2.Substring(i, 1)] = (int)ht[line2.Substring(i, 1)] - 1;;
                 }
             }
-            //for (int i=0;i<line1.Length; i++)
-            //{
-                //Console.WriteLine(ht[atoz.Substring(i,1)]);
-            //}
         }
     }
 }
