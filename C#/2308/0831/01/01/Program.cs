b5900131585f18235638d80b84471d8ab176fc09
Rm9tDVAM <u1exite.znd@gmail.com>
2023-08-31 13:00:46 +0900

    add

---
+++ C#/2308/0831/01/01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] src = { 0, 1, 2, 3, 4, 5 };
+            var query = src.Select(x => x % 3);
+            Console.WriteLine("[{0}]",string.Join(",",query));
+        }
+    }
+}
