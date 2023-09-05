cbe975ee8d2b3fe35b7f204bc0d9e9287418fe95
Rm9tDVAM <u1exite.znd@gmail.com>
2023-09-05 13:01:01 +0900

    add

---
+++ C#/2309/0905/01/01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int line1=int.Parse(Console.ReadLine());
+            int line2=int.Parse(Console.ReadLine());
+            Console.WriteLine((line1*2)+(line2*2));
+        }
+    }
+}
---
+++ C#/2309/0905/02_c083/02_c083/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_c083
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            List<int> list = new List<int>();
+            int max = 0;
+            int temp = 0;
+
+            for(int i = 0; i < int.Parse(line[0]); i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()) / int.Parse(line[1]));
+            }
+            max = list.Max();
+
+            for(int i = 0; i < list.Count; i ++)
+            {
+                temp = list[i];
+                for (int j = 0; j < max; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
