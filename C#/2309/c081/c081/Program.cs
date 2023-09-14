d919e44844068b7bb694bc06123ceae25ef81762
Rm9tDVAM <u1exite.znd@gmail.com>
2023-09-14 13:01:02 +0900

    add

---
+++ C#/2309/c081/c081/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c081
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int count = int.Parse(Console.ReadLine());
+            int count = 8;
+            List<string> list = new List<string>();
+            for(int i = 0; i < count; i++)
+            {
+                //list.Add(Console.ReadLine());
+            }
+            list.Add("A R");
+            list.Add("A L");
+            list.Add("");
+            list.Add("");
+            list.Add("");
+            list.Add("");
+            list.Add("");
+            list.Add("");
+
+        }
+    }
+}
