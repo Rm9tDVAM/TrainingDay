bb8444c7344990dfa15d99ab1b0e591c893a891c
Rm9tDVAM <u1exite.znd@gmail.com>
2023-09-15 13:01:07 +0900

    add

--- C#/2309/c081/c081/Program.cs
+++ C#/2309/c081/c081/Program.cs
@@ -17,15 +17,30 @@ namespace c081
             {
                 //list.Add(Console.ReadLine());
             }
+            int ans = 0;
+            string[] tempi;
+            string[] tempj;
             list.Add("A R");
             list.Add("A L");
-            list.Add("");
-            list.Add("");
-            list.Add("");
-            list.Add("");
-            list.Add("");
-            list.Add("");
+            list.Add("B R");
+            list.Add("A L");
+            list.Add("A R");
+            list.Add("B L");
+            list.Add("A L");
+            list.Add("C L");
+            for(int i = 0; i < list.Count; i++)
+            {
+                tempi = list[i].Split(' ');
+                for (int j = 0; j < list.Count; j++)
+                {
+                    tempj = list[j].Split(' ');
+                    if (tempi[0] == tempj[0] && tempi[1] != tempj[1]) 
+                    {
 
+
+                    }
+                }
+            }
         }
     }
 }
