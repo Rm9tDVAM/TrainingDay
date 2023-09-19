db7e6db9d6a037909327494fe8e69f6831b4ebb7
Rm9tDVAM <u1exite.znd@gmail.com>
2023-09-19 12:43:48 +0900

    add

--- C#/2309/c081/c081/Program.cs
+++ C#/2309/c081/c081/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,24 +11,16 @@ namespace c081
     {
         static void Main(string[] args)
         {
-            //int count = int.Parse(Console.ReadLine());
-            int count = 8;
+            int count = int.Parse(Console.ReadLine());
             List<string> list = new List<string>();
             for(int i = 0; i < count; i++)
             {
-                //list.Add(Console.ReadLine());
+                list.Add(Console.ReadLine());
             }
             int ans = 0;
             string[] tempi;
             string[] tempj;
-            list.Add("A R");
-            list.Add("A L");
-            list.Add("B R");
-            list.Add("A L");
-            list.Add("A R");
-            list.Add("B L");
-            list.Add("A L");
-            list.Add("C L");
+            RELOOP:
             for(int i = 0; i < list.Count; i++)
             {
                 tempi = list[i].Split(' ');
@@ -36,11 +29,14 @@ namespace c081
                     tempj = list[j].Split(' ');
                     if (tempi[0] == tempj[0] && tempi[1] != tempj[1]) 
                     {
-
-
+                        list.RemoveAt(j);
+                        list.RemoveAt(i);
+                        ans++;
+                        goto RELOOP;
                     }
                 }
             }
+            Console.WriteLine(ans);
         }
     }
 }
