9cdca73f5f2143ef8d315e941361f4d485c303bc
Rm9tDVAM <u1exite.znd@gmail.com>
2023-09-21 12:42:07 +0900

    add2

--- C#/2309/c080/c080/Program.cs
+++ C#/2309/c080/c080/Program.cs
@@ -16,20 +16,42 @@ namespace c080
             int line2 = 6;
             //string[] line3 = Console.ReadLine().Split(' ');
             //string[] line3 = {"1","1","3","4","1","2"};
-            string[] line3 = {"1","1","3","4","1","2","1","1","3","4","1","2"};
+            string[] line3 = {"1","1","3","4","1","2"};
             int buttonNum = int.Parse(line1[0]);
             int limit = int.Parse(line1[1]);
+            int limitPoint = 0;
+            int point = 0;
             for (int i=0; i < line3.Length; i++)
             {
                 if ((i + 1) % int.Parse(line1[0]) == 0)
                 {
-                    Console.WriteLine(line3[i]);
+                    if (line1[0] == line3[i])
+                    {
+                        point += 1000;
+                    }
+                    else
+                    {
+                        limitPoint++;
+                    }
+                    //Console.WriteLine(line1[0]);
                 }
                 else
+                {    if (line1[0] == line3[i])
+                    {
+                        point += 1000;
+                    }
+                    else
+                    {
+                        limitPoint++;
+                    }
+                    //Console.WriteLine((i + 1) % int.Parse(line1[0]));
+                }
+                if (limit == limitPoint)
                 {
-                    Console.WriteLine((i + 1) % int.Parse(line1[0]));
+                    Console.WriteLine(-1);
                 }
             }
+            Console.WriteLine(point);
         }
     }
 }
