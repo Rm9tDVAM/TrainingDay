4389166652e313384b65b848f31c0b30a74f5e9d
Rm9tDVAM <u1exite.znd@gmail.com>
2023-09-28 13:01:26 +0900

    add

---
+++ C#/2309/c079/c079/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c079
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] temp = Console.ReadLine().Split(' ');
+            string[] temp = { "6", "2" };
+            int count = int.Parse(temp[0]);
+            int cards = int.Parse(temp[1]);
+            Dictionary<int,int> map = new Dictionary<int,int>();
+            for (int i = 0; i < count; i++)
+            {
+                map.Add(i,0);
+            }
+            foreach (int i in map.Keys)
+            {
+                Console.WriteLine(map[i]);
+            }
+        }
+    }
+}
