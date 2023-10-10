08ee701a7e6ed3b464b53a1a4a7691ab16dca6d2
Rm9tDVAM <u1exite.znd@gmail.com>
2023-10-10 13:01:06 +0900

    add

---
+++ C#/2310/c078/c078/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int count = int.Parse(line[0]);
+            int min = int.Parse(line[1]);
+            int max = int.Parse(line[2]);
+            int ans = 0;
+            int nums = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if(num <= min)
+                {
+                    ans -= num;
+                    nums++;
+                }
+                else if (num >= max) {
+                    ans += num * nums;
+                    nums = 0;
+                }
+            }
+        }
+    }
+}
---
+++ C#/2310/d262/d262/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d262
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = Console.ReadLine();
+            string line2 = Console.ReadLine();
+            if(line1.Length == line2.Length)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
