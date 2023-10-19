be7b5b00fa6f808374975b99a329f1f7f3519568
Rm9tDVAM <u1exite.znd@gmail.com>
2023-10-19 12:59:09 +0900

    add

--- C#/2310/c078/c078/Program.cs
+++ C#/2310/c078/c078/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,29 +11,30 @@ namespace c078
     {
         static void Main(string[] args)
         {
-            //string[] line = Console.ReadLine().Split(' ');
-            string[] line = { "5","110","120" };
+            string[] line = Console.ReadLine().Split(' ');
             int count = int.Parse(line[0]);
             int min = int.Parse(line[1]);
             int max = int.Parse(line[2]);
             int ans = 0;
             int nums = 0;
-            int[] testdata = { 110,100,120,130,105 };
+            int num = 0;
 
             for (int i = 0; i < count; i++)
             {
-                //int num = int.Parse(Console.ReadLine());
-                int num = testdata[i];
+                num = int.Parse(Console.ReadLine());
                 if(num <= min)
                 {
                     ans -= num;
                     nums++;
                 }
-                else if (num >= max) {
+                else if (num >= max )
+                {
                     ans += num * nums;
                     nums = 0;
                 }
-                Console.WriteLine("{0} {1} {2}",num,nums,ans);
+            }
+            if ( nums != 0 ){
+                ans += num * nums;
             }
             Console.WriteLine(ans);
         }
