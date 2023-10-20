faf2ec0faff00f119a2ce68cd31a0d8233d98418
Rm9tDVAM <u1exite.znd@gmail.com>
2023-10-20 13:01:12 +0900

    add

---
+++ C#/2310/c077/c077/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c077
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] line = Console.ReadLine().Split(' ');
+            string[] line = { "3","25"};
+            int loop = int.Parse(line[0]);
+            int quest = int.Parse(line[1]);
+            int day = 0;
+            int ans = 0;
+            line[0] = "-2 17";
+            line[1] = "-2 17";
+            line[2] = "-2 17";
+
+            for ( int i = 1; i < loop; i++)
+            {
+                line=Console.ReadLine().Split(' ');
+                day= int.Parse(line[0]);
+                ans= int.Parse(line[1]);
+                if (day >= 10)
+                {
+                    Console.WriteLine("D");
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
---
+++ C#/2310/d264/d264/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d264
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int line = int.Parse(Console.ReadLine());
+            if (line >= 1000)
+            {
+                Console.WriteLine(line - 100);
+            }
+            else
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
