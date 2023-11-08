5e6ca13b12a675e2f22ab814fa041d96c49af1c7
Rm9tDVAM <u1exite.znd@gmail.com>
2023-11-08 12:59:39 +0900

    add

--- C#/2310/c076/c076/Program.cs
+++ C#/2310/c076/c076/Program.cs
@@ -11,19 +11,25 @@ namespace c076
     {
         static void Main(string[] args)
         {
-            string[] line1=Console.ReadLine().Split(' ');
-            //string[] line1 = { "1000","1300","1500" };
+            //string[] line1=Console.ReadLine().Split(' ');
+            string[] line1 = { "1000","1300","1500" };
+           // string[] line1 = { "1300","1500","1700" };
+            //string[] line1 = { "10","100","1000" };
             int payX = int.Parse(line1[0]);
             int payY = int.Parse(line1[1]);
             int payZ = int.Parse(line1[2]);
-            int day = int.Parse(Console.ReadLine());
+            //int day = int.Parse(Console.ReadLine());
             //int day = 4;
+            // int day = 7;
+            int day = 1;
             int ans = 0;
             //string[] temp = { "0 9","9 17","17 22","22 23" };
+            //string[] temp = { "8 19","9 20","10 21","11 22","0 23","20 22","0 21" };
+            string[] temp = { "9 17" };
             for(int i = 0; i < day; i++)
             {
-                string[] line2 = Console.ReadLine().Split(' ');
-                //string[] line2 = temp[i].Split(' ');
+                //string[] line2 = Console.ReadLine().Split(' ');
+                string[] line2 = temp[i].Split(' ');
                 int start = int.Parse(line2[0]);
                 int end = int.Parse(line2[1]);
                 //Console.WriteLine("{0},{1},{2}",i,start,end);
@@ -41,7 +47,7 @@ namespace c076
                     {
                         ans += payY;
                     }
-                    else
+                    else if (j == 23)
                     {
                         ans += payZ;
                     }
