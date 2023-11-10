d395d0967fe184e8aac17c4c6f1cabc429bd848d
Rm9tDVAM <u1exite.znd@gmail.com>
2023-11-10 12:57:41 +0900

    add

--- C#/2310/c076/c076/Program.cs
+++ C#/2310/c076/c076/Program.cs
@@ -11,45 +11,32 @@ namespace c076
     {
         static void Main(string[] args)
         {
-            //string[] line1=Console.ReadLine().Split(' ');
-            string[] line1 = { "1000","1300","1500" };
-           // string[] line1 = { "1300","1500","1700" };
-            //string[] line1 = { "10","100","1000" };
+            string[] line1=Console.ReadLine().Split(' ');
             int payX = int.Parse(line1[0]);
             int payY = int.Parse(line1[1]);
             int payZ = int.Parse(line1[2]);
-            int x = 0;
-            int y = 0;
-            int z = 0;
-            //int day = int.Parse(Console.ReadLine());
-            //int day = 4;
-            // int day = 7;
-            int day = 1;
-            //string[] temp = { "0 9","9 17","17 22","22 23" };
-            //string[] temp = { "8 19","9 20","10 21","11 22","0 23","20 22","0 21" };
-            string[] temp = { "9 17" };
+            int day = int.Parse(Console.ReadLine());
+            int ans = 0;
             for(int i = 0; i < day; i++)
             {
-                //string[] line2 = Console.ReadLine().Split(' ');
-                string[] line2 = temp[i].Split(' ');
+                string[] line2 = Console.ReadLine().Split(' ');
                 int start = int.Parse(line2[0]);
                 int end = int.Parse(line2[1]);
-                //Console.WriteLine("{0},{1},{2}",i,start,end);
                 for (int j = start; j < end; j++)
                 {
-                    if (j <= 9)
+                    if (j <= 8)
                     {
                         ans += payZ;
                     }
-                    else if (j <= 17)
+                    else if (j <= 16)
                     {
                         ans += payX;
                     }
-                    else if (j <= 22)
+                    else if (j <= 21)
                     {
                         ans += payY;
                     }
-                    else if (j == 23)
+                    else
                     {
                         ans += payZ;
                     }
---
+++ C#/2311/d266/d266/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d266
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = Console.ReadLine();
+            string line2 = Console.ReadLine();
+            Console.WriteLine("{0}{1}",line1,line2);
+        }
+    }
+}
