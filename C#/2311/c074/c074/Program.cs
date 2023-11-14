a7daa08c7a14c9aa11b1ea713597620677c377e1
Rm9tDVAM <u1exite.znd@gmail.com>
2023-11-14 12:59:41 +0900

    add

---
+++ C#/2311/c074/c074/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c074
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+
+        }
+    }
+}
---
+++ C#/2311/c075/c075/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c075
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line1 = Console.ReadLine().Split(' ');
+            int money = int.Parse(line1[0]);
+            int count = int.Parse(line1[1]);
+            int point = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int pay = int.Parse(Console.ReadLine());
+                if (point >= pay)
+                {
+                    point -= pay;
+                }
+                else if(money >= pay){
+                    point += pay / 10;
+                    money -= pay;
+                }
+                Console.WriteLine("{0} {1}",money,point);
+            }
+        }
+    }
+}
