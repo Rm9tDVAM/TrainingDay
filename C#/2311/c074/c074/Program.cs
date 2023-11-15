bf72b47aea3e015da1489c9ff53b09ca3ed123f1
Rm9tDVAM <u1exite.znd@gmail.com>
2023-11-15 13:00:55 +0900

    add

--- C#/2311/c074/c074/Program.cs
+++ C#/2311/c074/c074/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,20 @@ namespace c074
     {
         static void Main(string[] args)
         {
-            string[] line = Console.ReadLine().Split(' ');
-
+            //string[] line = Console.ReadLine().Split(' ');
+            string[] line = { "6","8","6" };
+            int count = int.Parse(line[0]);
+            int length = int.Parse(line[2]);
+            string tmp="";
+            string[] temp = { "My#name#","is#paiza",".#I#like","#program","ming#ver","y#much.#" };
+            for (int i = 0; i < count; i++)
+            {
+                //tmp = tmp + Console.ReadLine();
+                tmp = tmp + temp[i];
+            }
+            for(int i = 0;i < tmp.Length; i + int.Parse(line[2]))
+            {
+            }
         }
     }
 }
