e7212a8cb029a8c6b865a20a9be42bacc98fd42f
Rm9tDVAM <u1exite.znd@gmail.com>
2023-11-16 13:03:40 +0900

    add

--- C#/2311/c074/c074/Program.cs
+++ C#/2311/c074/c074/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,14 +18,18 @@ namespace c074
             int count = int.Parse(line[0]);
             int length = int.Parse(line[2]);
             string tmp="";
-            string[] temp = { "My#name#","is#paiza",".#I#like","#program","ming#ver","y#much.#" };
+            string[] temp = { "My#name#","is#paiza",".#I#like","#program","ming#ver","y#ch.#" };
             for (int i = 0; i < count; i++)
             {
                 //tmp = tmp + Console.ReadLine();
                 tmp = tmp + temp[i];
             }
-            for(int i = 0;i < tmp.Length; i + int.Parse(line[2]))
+            for (int i = 0; i < tmp.Length; i += int.Parse(line[2]) ) 
             {
+                //Console.WriteLine("{0},{1}",i,tmp.Length);
+                //Console.WriteLine(tmp.Substring(i, int.Parse(line[2])));
+                //Console.WriteLine(tmp.Substring(i, tmp.Length));
+                Console.WriteLine(tmp.Substring(i,1));
             }
         }
     }
