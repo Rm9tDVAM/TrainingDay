b6c691181a6ab81a43b39e24a3c96ef29d1fa3d1
Rm9tDVAM <u1exite.znd@gmail.com>
2023-11-20 13:01:09 +0900

    add

--- C#/2311/c073/c073/Program.cs
+++ C#/2311/c073/c073/Program.cs
@@ -10,6 +10,29 @@ namespace c073
     {
         static void Main(string[] args)
         {
+            //int L = int.Parse(Console.ReadLine());
+            int L = 6;
+            /*string[] uab=Console.ReadLine().Split(' ');
+            int u = int.Parse(uab[0]);
+            int a = int.Parse(uab[1]);
+            int b = int.Parse(uab[2]);
+            */
+            int u = 2;
+            int a = 2;
+            int b = 5;
+            //int v = int.Parse(Console.ReadLine());
+            int v = 4;
+            int usagi = (u*L)+(L/a*5);
+            int kame = v * L;
+            Console.WriteLine("{0},{1}",usagi,kame);
+            if (usagi > kame)
+            {
+                Console.WriteLine("KAMEA");
+            }
+            else
+            {
+                Console.WriteLine("USAGI");
+            }
         }
     }
 }
