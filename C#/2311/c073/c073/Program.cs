5ce4abfbcf96bfa2c5ad4e806e8536864334fc0f
Rm9tDVAM <u1exite.znd@gmail.com>
2023-11-28 13:01:02 +0900

    add

--- C#/2311/c073/c073/Program.cs
+++ C#/2311/c073/c073/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,28 +11,33 @@ namespace c073
     {
         static void Main(string[] args)
         {
-            //int L = int.Parse(Console.ReadLine());
-            int L = 6;
-            /*string[] uab=Console.ReadLine().Split(' ');
+            int L = int.Parse(Console.ReadLine());
+            string[] uab=Console.ReadLine().Split(' ');
             int u = int.Parse(uab[0]);
             int a = int.Parse(uab[1]);
             int b = int.Parse(uab[2]);
-            */
-            int u = 2;
-            int a = 2;
-            int b = 5;
-            //int v = int.Parse(Console.ReadLine());
-            int v = 4;
-            int usagi = (u*L)+(L/a*5);
-            int kame = v * L;
-            Console.WriteLine("{0},{1}",usagi,kame);
-            if (usagi > kame)
+            int v = int.Parse(Console.ReadLine());
+            int usagi = 0;
+            if (L % a == 0)
             {
-                Console.WriteLine("KAMEA");
+                usagi = (u * L) + (L / a * b) -b;
             }
             else
             {
+                usagi = (u * L) + (L / a * b);
+            }
+            int kame = v * L;
+            if (usagi > kame)
+            {
+                Console.WriteLine("KAME");
+            }
+            else if(usagi < kame)
+            {
                 Console.WriteLine("USAGI");
+            }
+            else
+            {
+                Console.WriteLine("DRAW");
             }
         }
     }
