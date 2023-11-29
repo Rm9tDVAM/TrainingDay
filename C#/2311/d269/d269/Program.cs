5474d2d09065f5bb935646ca1f5594ffbcbd6c2c
Rm9tDVAM <u1exite.znd@gmail.com>
2023-11-29 13:00:34 +0900

    add

---
+++ C#/2311/d269/d269/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d269
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();  
+            for(int i=0;i<line.Length; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.Write(line[i]);
+                }
+            }
+        }
+    }
+}
