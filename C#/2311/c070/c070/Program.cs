087d3956ca44239fb4c78d40a6779a730782bfe1
Rm9tDVAM <u1exite.znd@gmail.com>
2023-12-01 13:01:14 +0900

    add

--- C#/2311/c070/c070/Program.cs
+++ C#/2311/c070/c070/Program.cs
@@ -10,6 +10,19 @@ namespace c070
     {
         static void Main(string[] args)
         {
+            int line =int.Parse(Console.ReadLine());
+            for (int i = 0; i < line; i++)
+            {
+                string line2=Console.ReadLine();
+                if(4==line2.Count(f => (f == char.Parse(line2.Substring(0, 1)))))
+                {
+                    Console.WriteLine("Four Card");
+                }
+                for (int j=0;j<line2.Length; j++)
+                {
+                }
+
+            }
         }
     }
 }
