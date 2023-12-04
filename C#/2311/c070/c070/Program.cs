765ed104f809a1007f9e909724dc0a3c0a8cbb91
Rm9tDVAM <u1exite.znd@gmail.com>
2023-12-04 13:00:39 +0900

    add

--- C#/2311/c070/c070/Program.cs
+++ C#/2311/c070/c070/Program.cs
@@ -11,17 +11,38 @@ namespace c070
         static void Main(string[] args)
         {
             int line =int.Parse(Console.ReadLine());
+            int pairCount = 0;
             for (int i = 0; i < line; i++)
             {
                 string line2=Console.ReadLine();
-                if(4==line2.Count(f => (f == char.Parse(line2.Substring(0, 1)))))
+                if (4 == line2.Count(f => (f == char.Parse(line2.Substring(0, 1)))))
                 {
                     Console.WriteLine("Four Card");
                 }
+                else if (3 == line2.Count(f => (f == char.Parse(line2.Substring(0, 1))))||
+                         3 == line2.Count(f => (f == char.Parse(line2.Substring(1, 1))))) 
+                {
+                    Console.WriteLine("Three Card");
+                }
                 for (int j=0;j<line2.Length; j++)
                 {
+                    if (2 == line2.Count(f => (f == char.Parse(line2.Substring(i, 1)))))
+                    {
+                        pairCount++;
+                    }
                 }
-
+                if ( pairCount == 4 )
+                {
+                    Console.WriteLine("Two Pair");
+                }
+                else if( pairCount == 2 )
+                {
+                    Console.WriteLine("One Pair");
+                }
+                else
+                {
+                    Console.WriteLine("No Pair");
+                }
             }
         }
     }
