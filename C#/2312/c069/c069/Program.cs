7a8040eb344a1f973a72344fe42858ad83a8e30c
Rm9tDVAM <u1exite.znd@gmail.com>
2023-12-07 13:01:34 +0900

    add

---
+++ C#/2312/c069/c069/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c069
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] ymd = Console.ReadLine().Split(' ');
+            //string[] ab = Console.ReadLine().Split(' ');
+            //int y = int.Parse(ymd[0]);
+            int y = 2000;
+            //int m = int.Parse(ymd[1]);
+            //int d = int.Parse(ymd[2]);
+            //int a = int.Parse(ab[0]);
+            //int b = int.Parse(ab[1]);
+            Console.WriteLine((y)%4);
+        }
+    }
+}
