6a5d4e136b4ff773a6b02d38a2c250d13313c98c
Rm9tDVAM <u1exite.znd@gmail.com>
2023-12-18 13:00:54 +0900

    add

--- C#/2312/c072/c072/Program.cs
+++ C#/2312/c072/c072/Program.cs
@@ -10,14 +10,31 @@ namespace c072
     {
         static void Main(string[] args)
         {
-            string[] line= Console.ReadLine().Split(' ');  
+            List<string> list = new List<string>();
+            //string[] line = Console.ReadLine().Split(' ');  
+            string[] line = { "100","150","200" };
             int ATK = int.Parse(line[0]);
             int DEF = int.Parse(line[1]);
             int AGI = int.Parse(line[0]);
-            int N = int.Parse(Console.ReadLine());
+            //int N = int.Parse(Console.ReadLine());
+            int N = 3;
+            string[] testData{ };
             for(int i = 0; i < N; i++)
             {
+                string[] line2 = Console.ReadLine().Split(' ');
+                string s = line2[0];
+                int MINATK = int.Parse(line2[1]);
+                int MAXATK = int.Parse(line2[1]);
+                int MINDEF = int.Parse(line2[1]);
+                int MAXDEF = int.Parse(line2[1]);
+                int MINAGI = int.Parse(line2[1]);
+                int MAXAGI = int.Parse(line2[1]);
 
+
+            }
+            if( list.Count == 0 )
+            {
+                Console.WriteLine("no evolution");
             }
         }
     }
