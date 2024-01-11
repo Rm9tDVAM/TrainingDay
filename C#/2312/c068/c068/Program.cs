10b9207e43b457d0dc2e281b4c0c86a7bc6f4dcf
Rm9tDVAM <u1exite.znd@gmail.com>
2024-01-11 12:57:38 +0900

    add

--- C#/2312/c068/c068/Program.cs
+++ C#/2312/c068/c068/Program.cs
@@ -12,10 +12,12 @@ namespace c068
         {
             //int N = int.Parse(Console.ReadLine());
             //int N = 4;
-            int N = 19;
+            //int N = 19;
+            int N = 2;
             //string S = Console.ReadLine();
             //string S = "QEPG";
-            string S = "KXKPMQVI";
+            //string S = "KXKPMQVI";
+            string S = "B";
             // A=65, Z=90
             for(int i=0; i<S.Length; i++)
             {
@@ -23,16 +25,18 @@ namespace c068
                 {
                     if ( 65 > (int)char.Parse(S.Substring(i,1))-N)
                     {
-                        Console.Write(65 - ((int)char.Parse(S.Substring(i,1))-N));
+                        Console.WriteLine(95 - ( 66 - (int)char.Parse(S.Substring(i,1))-N));
+
+                        //Console.WriteLine(65 - ((int)char.Parse(S.Substring(i,1))-N));
                     }
                     else
                     {
-                        Console.Write((char)((int)char.Parse(S.Substring(i,1))-N));
+                        //Console.Write((char)((int)char.Parse(S.Substring(i,1))-N));
                     }
                 }
                 else
                 {
-                    Console.Write((char)((int)char.Parse(S.Substring(i,1))+N));
+                    //Console.Write((char)((int)char.Parse(S.Substring(i,1))+N));
                 }
             }
         }
