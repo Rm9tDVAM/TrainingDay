3ad1b1c2c2d7630c79ad5b980e20cfdabcba3749
Rm9tDVAM <u1exite.znd@gmail.com>
2024-02-09 12:42:31 +0900

    add

--- C#/2312/c068/c068/Program.cs
+++ C#/2312/c068/c068/Program.cs
@@ -16,46 +16,30 @@ namespace c068
             {
                 az.Add (c);
             }
-            int N = 1;
-            //string S = Console.ReadLine();
-            string S = "QEPG";
-            //string S = "KXKPMQVI";
-            //string S = "AA";
-            // A=az[0], Z=az[25]
+            int N = int.Parse(Console.ReadLine());
+            string S = Console.ReadLine();
             for(int i=0; i<S.Length; i++)
             {
                 if(i%2 == 0)
                 {
                     if (az.IndexOf(S[i])-(N) < 0)
                     {
-                        /*
-                        Console.WriteLine(true);
-                        Console.WriteLine(az.IndexOf(S[i])-(N));
-                        Console.WriteLine(26 + (az.IndexOf(S[i])-(N)));
-                        */
-                        Console.WriteLine(az[26 + (az.IndexOf(S[i])-(N))]);
+                        Console.Write(az[26 + (az.IndexOf(S[i])-(N))]);
                     }
                     else
                     {
-                        /*
-                        Console.WriteLine(false);
-                        */
-                        Console.WriteLine(az[az.IndexOf(S[i])-(N)]);
+                        Console.Write(az[az.IndexOf(S[i])-(N)]);
                     }
                 }
                 else
                 {
-                    //Console.WriteLine(az.IndexOf(S[i])+N);
                     if (az.IndexOf(S[i])+(N) > 25)
                     {
-                        //Console.WriteLine(true);
-                        //Console.WriteLine((az.IndexOf(S[i])+(N))-26);
-                        Console.WriteLine(az[(az.IndexOf(S[i])+(N))-26]);
+                        Console.Write(az[(az.IndexOf(S[i])+(N))-26]);
                     }
                     else
                     {
-                        //Console.WriteLine(false);
-                        Console.WriteLine(az[az.IndexOf(S[i])+(N)]);
+                        Console.Write(az[az.IndexOf(S[i])+(N)]);
                     }
                 }
             }
