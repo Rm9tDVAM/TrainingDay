da9cc7a4e886465a489530d311c0bb8686bb1df5
Rm9tDVAM <u1exite.znd@gmail.com>
2024-02-16 13:00:46 +0900

    add

---
+++ C#/2402/d274/d274/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d274
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            if ( N >= a)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    }
+}
---
+++ C#/2402/d276/d276/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d276
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int add = 0;
+            if ( M % 30 > 0)
+            {
+                add++;
+            }
+            Console.WriteLine(X*((M/30)+add));
+        }
+    }
+}
