e0cd239dad67fd3ffa8403e01bf86349fc2dabf1
Rm9tDVAM <u1exite.znd@gmail.com>
2024-02-16 12:43:23 +0900

    add

---
+++ C#/2402/d273/d273/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d273
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int M = int.Parse(Console.ReadLine());
+            M = M += 2;
+            if (M > 12) 
+            {
+                Console.WriteLine(M-12);
+            }
+            else
+            {
+                Console.WriteLine(M);
+            }
+        }
+    }
+}
