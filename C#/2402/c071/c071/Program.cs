a8d2045576f88fc8c884cd532da761c3e27ce8d0
Rm9tDVAM <u1exite.znd@gmail.com>
2024-02-27 12:44:33 +0900

    add

---
+++ C#/2402/c071/c071/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c071
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int M = int.Parse(line[0]); // 1<= M <=30
+            int N = int.Parse(line[1]); // 1<= N <=30
+            int num = 0;
+            int ans = 0;
+            for(int i = 1; i < M; i++)
+            {
+                for(int j = 1; j < N; j++) { 
+                    if(!(i == j))
+                    {
+                        num = (i*i) + (j*j);
+                        for(int k = 1; k < num; k++)
+                        {
+                            if (k * k == num)
+                            {
+                                ans++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ C#/2402/d280/d280/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d280
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int W = int.Parse(line[0]);
+            int M = int.Parse(line[1]);
+            Console.WriteLine(W/60*M);
+        }
+    }
+}
