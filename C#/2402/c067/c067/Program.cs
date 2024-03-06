8ce9e8df041227d167d697b43723daa2a0aa5a67
Rm9tDVAM <u1exite.znd@gmail.com>
2024-03-06 12:52:56 +0900

    add

--- C#/2402/c067/c067/Program.cs
+++ C#/2402/c067/c067/Program.cs
@@ -14,25 +14,19 @@ namespace c067
             // 1 ≦ k_i ≦ 32 (1 ≦ i ≦ N)
             // 1 ≦ X < 2^31
             string[] line = Console.ReadLine().Split(' ');
-            //string[] line = { "3", "44" };
             int N = int.Parse(line[0]);
-            //int N = 1;
             string binary = Convert.ToString(int.Parse(line[1]),2);
-            Console.WriteLine(binary);
             List<char> list = new List<char>();
-            for (int i =0; i < binary.Length; i++)
+
+            for (int i = binary.Length-1; i >= 0; i--)
             {
                 list.Add(binary[i]);
-            }
-            foreach (char c in list) {
-                Console.WriteLine(c);
             }
 
             int k = 0;
             for (int i = 0; i < N; i++)
             {
-                k = int.Parse(Console.ReadLine())-1;
-                Console.WriteLine(list[k]);
+                Console.WriteLine(list[int.Parse(Console.ReadLine())-1]);
             }
         }
     }
