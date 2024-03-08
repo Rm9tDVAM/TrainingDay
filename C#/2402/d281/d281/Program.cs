b45767d8ea32904a1105f3856e10648bb3bf255e
Rm9tDVAM <u1exite.znd@gmail.com>
2024-03-08 12:41:48 +0900

    add

---
+++ C#/2402/d281/d281/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d281
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            string[] line = Console.ReadLine().Split(' ');
+            int Y = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                Y += int.Parse(line[i]);
+            }
+            if (X <= Y)
+            {
+                Console.WriteLine("NG");
+            }
+            else
+            {
+                Console.WriteLine("OK");
+            }
+        }
+    }
+}
---
+++ C#/2402/d282/d282/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d282
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            Console.WriteLine(M / N);
+        }
+    }
+}
