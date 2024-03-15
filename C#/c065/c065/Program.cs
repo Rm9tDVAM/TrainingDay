1ebd690ba04da344b4d0c762c6380366aa78b2c7
Rm9tDVAM <u1exite.znd@gmail.com>
2024-03-15 13:01:09 +0900

    add

---
+++ C#/c065/c065/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c065
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int N = int.Parse(Console.ReadLine());
+            int N = 3;
+            int ans = 0;
+            List<string> list = new List<string>();
+            for ( int i = 0; i < N; i++ )
+            {
+                //list.Add(Console.ReadLine());
+            }
+            list.Add("> 30");
+            list.Add("< 40");
+            list.Add("/ 5");
+            for ( int i = 1;i < 100; i++)
+            {
+                foreach ( string s in list)
+                {
+                if (s.IndexOf(">") != -1 )
+                    {
+
+                    }
+
+                }
+            }
+        }
+    }
+}
