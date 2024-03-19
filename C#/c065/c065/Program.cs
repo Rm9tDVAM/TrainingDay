28b96dc8cfbe8286f3b0f660bb0ed6fbadaaf347
Rm9tDVAM <u1exite.znd@gmail.com>
2024-03-19 12:47:50 +0900

    add

--- C#/c065/c065/Program.cs
+++ C#/c065/c065/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,41 +14,46 @@ namespace c065
             // 1 ≦ N ≦ 10
             // o_i は半角記号で ">", "<", "/" のいずれか
             // 1 ≦ x_i ≦ 100 
-            //int N = int.Parse(Console.ReadLine());
-            int N = 3;
+            int N = int.Parse(Console.ReadLine());
             int cnt = 0;
             List<string> list = new List<string>();
             for ( int i = 0; i < N; i++ )
             {
-                //list.Add(Console.ReadLine());
+                list.Add(Console.ReadLine());
             }
-            list.Add("> 30");
-            list.Add("< 40");
-            list.Add("/ 5");
             for ( int i = 1;i < 100; i++)
             {
                 for ( int j = 0;j<list.Count; j++)
                 {
-                    if (s.IndexOf(">") != -1 )
+                    if (list[j].IndexOf(">") != -1 )
                     {
-                        if ( i > int.Parse(s.Substring(s.IndexOf(" ") + 1)))
+                        if ( i > int.Parse(list[j].Substring(list[j].IndexOf(" ") + 1)))
                         {
                             cnt++;
                         }
                     }
-                    else if (s.IndexOf("<") != -1 )
+                    else if (list[j].IndexOf("<") != -1 )
                     {
-                        if ( i < int.Parse(s.Substring(s.IndexOf(" ") + 1)))
+                        if ( i < int.Parse(list[j].Substring(list[j].IndexOf(" ") + 1)))
                         {
                             cnt++;
                         }
                     }
-                    else if (s.IndexOf("/") != -1 )
+                    else if (list[j].IndexOf("/") != -1 )
                     {
-                        if ( i % int.Parse(s.Substring(s.IndexOf(" ") + 1)) == 0 )
+                        if ( i % int.Parse(list[j].Substring(list[j].IndexOf(" ") + 1)) == 0 )
                         {
                             cnt++;
                         }
+                    }
+                    if ( j == list.Count - 1)
+                    {
+                        if ( list.Count == cnt)
+                        {
+                            Console.WriteLine(i);
+                            Environment.Exit(0);
+                        }
+                        cnt = 0;
                     }
                 }
             }
