f554afa750a01a6a3f7ce5b3dc8ed0bfc7e5788f
Rm9tDVAM <u1exite.znd@gmail.com>
2024-03-21 12:25:54 +0900

    add

---
+++ C#/d283/d283/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d283
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int X = int.Parse(line[0]);
+            int P = int.Parse(line[1]);
+            if (X - P <= 0)
+            {
+                Console.WriteLine(0);
+
+            }
+            else
+            {
+                Console.WriteLine(X - P);
+            }
+        }
+    }
+}
