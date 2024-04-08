edda9f39f995de1fdd8c699f5a915fb1c70ea3b1
Rm9tDVAM <u1exite.znd@gmail.com>
2024-04-08 12:59:50 +0900

    add

---
+++ C#/c063/c063/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c063
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //・1 ≦ N ≦ 30
+            //・1 ≦ a_i, b_i ≦ 30(1 ≦ i ≦ N)
+
+            //int N = int.Parse(Console.ReadLine());
+            int N = 4;
+            int a = 0;
+            int b = 0;
+            int[] days = new int[60];
+            int max = 0;
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < N; i++) 
+            {
+                //list.Add(Console.ReadLine());
+            }
+            //list.Add("1 3");list.Add("2 2");list.Add("1 1");list.Add("1 2");
+            list.Add("1 3");list.Add("2 2");list.Add("2 1");list.Add("1 2");
+
+            for (int i = 0;i < list.Count; i++)
+            {
+                a = int.Parse(list[i].Substring(0,1));
+                b = int.Parse(list[i].Substring(2,1));
+                days[i + b] += a;
+            }
+
+            foreach (int i in days)
+            {
+                max = Math.Max(max, i);
+            }
+
+            for (int i = 0;i < days.Length ; i++)
+            {
+                if (days[i] == max)
+                {
+                    Console.WriteLine(i + 1);
+                }
+            }
+        }
+    }
+}
