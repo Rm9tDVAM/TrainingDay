d73ef38dab1fcd70f50ca9e56e754eea23f76a59
Rm9tDVAM <u1exite.znd@gmail.com>
2024-05-27 12:40:42 +0900

    add

---
+++ C#/d291/d291/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d291
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(y / x);
+            Console.WriteLine(y % x);
+        }
+    }
+}
