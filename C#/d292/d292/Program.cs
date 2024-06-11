b2cd2545e9d4fd37e9e3a2d27bdbbe588b8bcb44
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-11 12:36:26 +0900

    add

---
+++ C#/d292/d292/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d292
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 0; i < 7; i++) { 
+                if ( Console.ReadLine() == "Rain")
+                {
+                    count++;
+                }
+            }
+            if ( count >= 4 )
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
