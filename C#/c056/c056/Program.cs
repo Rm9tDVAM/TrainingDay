2fcc966fc7e8952bc0b0d405a24f80407fbe82ae
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-12 12:59:01 +0900

    add

---
+++ C#/c056/c056/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c056
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int N = int.Parse(line[0]);
+            int M = int.Parse(line[1]);
+            int a_i = 0;
+            int b_i = 0;
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                list.Add(line[i]);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            { 
+
+            }
+        }
+    }
+}
