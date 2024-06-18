cce499202afda86089d4da088ae9e8cd2fa9c3ce
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-18 12:59:20 +0900

    add

---
+++ C#/c055/c055/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c055
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string G = Console.ReadLine();
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+            }
+        }
+    }
+}
