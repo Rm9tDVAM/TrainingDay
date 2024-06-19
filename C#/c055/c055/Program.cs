b889a452fb31eef917dc8d0eec88718c8fab1cea
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-19 12:25:49 +0900

    add

--- C#/c055/c055/Program.cs
+++ C#/c055/c055/Program.cs
@@ -13,6 +13,7 @@ namespace c055
             int N = int.Parse(Console.ReadLine());
             string G = Console.ReadLine();
             List<string> list = new List<string>();
+            int cnt = 0;
 
             for (int i = 0; i < N; i++)
             {
@@ -21,6 +22,19 @@ namespace c055
 
             for (int i = 0; i < list.Count; i++)
             {
+                if (list[i].Contains(G))
+                {
+                    Console.WriteLine(list[i]);
+                }
+                else
+                {
+                    cnt++;
+                }
+            }
+
+            if (cnt == N)
+            {
+                Console.WriteLine("None");
             }
         }
     }
