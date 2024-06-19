deaaccb0267b19cf91e16b42dd7ffcbc5003aff1
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-19 13:00:52 +0900

    add

---
+++ C#/c054/c054/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c054
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int N = int.Parse(line[0]);
+            int V = int.Parse(line[1]);
+            int t_i = 0;
+            int p_i = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                line = Console.ReadLine().Split(' ');
+                t_i = int.Parse(line[0]);
+                p_i = int.Parse(line[1]);
+            }
+        }
+    }
+}
