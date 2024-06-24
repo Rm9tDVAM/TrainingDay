a63d8f7239427c05e3bb19c1dee666594f8ac76c
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-24 12:55:25 +0900

    add

--- C#/c054/c054/Program.cs
+++ C#/c054/c054/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,17 +17,38 @@ namespace c054
             int V = int.Parse(line[1]);
             int t_i = 0;
             int p_i = 0;
+            int t_itmp = 0;
+            int p_itmp = 0;
+            int sum = 0;
+            bool ans = false;
 
             for (int i = 0; i < N; i++)
             {
                 list.Add(Console.ReadLine());
             }
 
-            for (int i = 1; i < list.Count; i++)
+            for (int i = 0; i < list.Count-1; i++)
             {
-                line = list[i - 1].Split(' ');
+                line = list[i].Split(' ');
                 t_i = int.Parse(line[0]);
                 p_i = int.Parse(line[1]);
+                line = list[i+1].Split(' ');
+                t_itmp = int.Parse(line[0]);
+                p_itmp = int.Parse(line[1]);
+                sum = (p_itmp - p_i) / (t_itmp - t_i);
+                if (sum > V)
+                {
+                    ans = true;
+                }
+            }
+
+            if (ans) 
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
             }
         }
     }
