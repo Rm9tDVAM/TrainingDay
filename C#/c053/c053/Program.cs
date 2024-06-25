c93659e2b07451ca312f6989c0ec1b87f996e0f9
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-25 12:54:24 +0900

    add

---
+++ C#/c053/c053/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c053
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] list = Console.ReadLine().Split(' ');
+            }
+        }
+    }
+}
