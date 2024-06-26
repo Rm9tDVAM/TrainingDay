23c63dbec659ed7e38e776243ff8eb0e0b21f9d7
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-26 12:56:32 +0900

    add

--- C#/c053/c053/Program.cs
+++ C#/c053/c053/Program.cs
@@ -11,8 +11,35 @@ namespace c053
         static void Main(string[] args)
         {
             int N = int.Parse(Console.ReadLine());
-            string[] list = Console.ReadLine().Split(' ');
+            int sum = 0;
+            int tenTimes = 1;
+            string[] tmpList = Console.ReadLine().Split(' ');
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < tmpList.Length; i++) 
+            {
+                if (tmpList[i] == "x10")
+                {
+                    tenTimes = 10;
+                }
+                else
+                {
+                    list.Add(int.Parse(tmpList[i]));
+                }
             }
+
+            list.Sort();
+            if (list[0] == 0) 
+            {
+                list[list.Count -1] = 0;
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += list[i];
+            }
+
+            Console.WriteLine(sum * tenTimes);
         }
     }
 }
