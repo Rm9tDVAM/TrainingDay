4f3b917c8d6ae300166875b52ec385e9b4dc2e9d
Rm9tDVAM <u1exite.znd@gmail.com>
2024-06-27 13:00:48 +0900

    add

---
+++ C#/c052/c052/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c052
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //・1 ≦ H, W ≦ 10,000
+            //・-H ≦ dy ≦ H
+            //・-W ≦ dx ≦ W
+            string[] line = Console.ReadLine().Split(' ');
+            int H = int.Parse(line[0]);
+            int W = int.Parse(line[1]);
+            line = Console.ReadLine().Split(' ');
+            int dy = int.Parse(line[0]);
+            int dx = int.Parse(line[0]);
+
+            Console.WriteLine(H * dy);
+            Console.WriteLine(W * dx);
+            Console.WriteLine(H * dy + W * dx);
+            Console.WriteLine(dy * dx);
+            Console.WriteLine(H * dy + W * dx - dy * dx);
+        }
+    }
+}
