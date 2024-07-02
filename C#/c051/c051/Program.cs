a417f5c0758d3a921e3d11b1bb78eaa69c73c58c
Rm9tDVAM <u1exite.znd@gmail.com>
2024-07-02 12:49:12 +0900

    add

---
+++ C#/c051/c051/Program.cs
@@ -0,0 +1,36 @@
+﻿using System; using System.Collections.Generic; using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c051
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            List<int> list = new List<int>();
+            int sum = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                list.Add(int.Parse(line[i]));
+            }
+
+            list.Sort((a, b) => b - a);
+            for(int i = 0;i < list.Count;i++) 
+            {
+                if (i == 0 || i == 1) 
+                {
+                    sum += list[i] * 10;
+                }
+
+                else
+                {
+                    sum += list[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
