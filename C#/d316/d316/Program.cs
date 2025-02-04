cde08953ac0e9769c9c45120690be4a022c83a0e
Rm9tDVAM <u1exite.znd@gmail.com>
2025-02-04 12:58:36 +0900

    add

---
+++ C#/d316/d316/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d316
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 30;
+            int t = 0;
+            for (int i = 0; i < 4; i++) {
+                t = int.Parse(Console.ReadLine());
+                min = Math.Min(t, min);
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
