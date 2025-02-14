e77e18fcab12da735dd65fcdaa67555655490943
Rm9tDVAM <u1exite.znd@gmail.com>
2025-02-14 12:57:48 +0900

    add

--- C#/d320/d320/Program.cs
+++ C#/d320/d320/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.IO.Pipes;
 using System.Linq;
 using System.Text;
@@ -14,7 +13,6 @@ namespace d320
         {
             int n = int.Parse(Console.ReadLine());
             int ans = n / 6;
-            Console.WriteLine(ans);
             if ( n % 6 == 0) {
             }
             else
