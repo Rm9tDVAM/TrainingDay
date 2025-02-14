56fe3e19d2e286f4759f793ebd7c09de1f656a4c
Rm9tDVAM <u1exite.znd@gmail.com>
2025-02-14 12:51:08 +0900

    add

---
+++ C#/d320/d320/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d320
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int ans = n / 6;
+            Console.WriteLine(ans);
+            if ( n % 6 == 0) {
+            }
+            else
+            {
+                ans++;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
